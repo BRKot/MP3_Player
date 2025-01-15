27a5db287512b3fdb5c2597a38abcf3d41ba3177
BRKot <65824266+BRKot@users.noreply.github.com>
2025-01-15 21:14:47 +0500

    Создание окна плеера

--- MP3_Player/MP3_Player/PlayerViewController.swift
+++ MP3_Player/MP3_Player/PlayerViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 protocol PlayerView{
     func configurCoverImageView(image: UIImage)
@@ -13,7 +14,7 @@ protocol PlayerView{
     func configurTrackNameLabel(name: String)
     func configurAuthorNameLabel(name: String)
     
-    func configurTrackTimeSlider(image: UIImage)
+    func configurTrackTimeSlider(value: Float)
     
     func configurTrackBackButton()
     func configurPlayTrackButton(image: UIImage)
@@ -21,29 +22,38 @@ protocol PlayerView{
 }
 
 class PlayerViewController: UIViewController {
-
-    @IBOutlet var TrackCoverUIImageView: UIImageView!
+    @IBOutlet var trackCoverUIImageView: UIImageView!
     
     @IBOutlet var TrackNameLabel: UILabel!
     @IBOutlet var AuthorNameLabel: UILabel!
     
-    @IBOutlet var TrackTimeSlider: UISlider!
+    @IBOutlet var trackTimeSlider: UISlider!
     
     @IBOutlet var TrackBackButton: UIButton!
-    @IBOutlet var PlayTrackButton: UIButton!
+    @IBOutlet var playTrackButton: UIButton!
     @IBOutlet var TrackForwardButton: UIButton!
     
     var presenter: PlayerViewPresenter?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        presenter = PlayerViewPresenter(view: self)
+        presenter?.setupUI()
+    }
+    
+    @IBAction func onPlayButtonTuch(_ sender: Any) {
+        presenter?.isPlaing = !presenter!.isPlaing
+    }
+    
+    @IBAction func changeValueSlider(_ sender: Any) {
+        presenter?.changeTrackTime(partFullTime: Double(self.trackTimeSlider.value))
     }
 }
 
 extension PlayerViewController: PlayerView{
     func configurCoverImageView(image: UIImage) {
-        
+        self.trackCoverUIImageView.layer.cornerRadius = trackTimeSlider.frame.width / 15
+        self.trackCoverUIImageView.image = image
     }
     
     func configurTrackNameLabel(name: String) {
@@ -54,8 +64,8 @@ extension PlayerViewController: PlayerView{
         
     }
     
-    func configurTrackTimeSlider(image: UIImage) {
-        
+    func configurTrackTimeSlider(value: Float) {
+        self.trackTimeSlider.value = value
     }
     
     func configurTrackBackButton() {
@@ -63,13 +73,11 @@ extension PlayerViewController: PlayerView{
     }
     
     func configurPlayTrackButton(image: UIImage) {
-        
-    }
+          self.playTrackButton.setImage(image, for: .normal)
+          self.playTrackButton.tintColor = UIColor.white
+      }
     
     func configurTrackForwardButton() {
         
     }
-    
-
 }
-
--- MP3_Player/MP3_Player/PlayerViewPresenter.swift
+++ MP3_Player/MP3_Player/PlayerViewPresenter.swift
@@ -6,15 +6,89 @@
 //
 
 import Foundation
+import UIKit
+import AVFoundation
 
 class PlayerViewPresenter{
     
-    init(){
-        
+
+    private var view: PlayerView?
+    var timeObserver: Any?
+    var audioPlayer: AVPlayer?
+    var newTrack = true
+    var isPlaing: Bool = false {
+        didSet{
+            tupOnPlayButton()
+        }
+    }
+    
+    init(view: PlayerView){
+        self.view = view
     }
     
     func setupUI(){
+        guard let playIcon = UIImage(systemName: "play.fill") else {
+            print("Иконка не найдена")
+            return
+        }
+        print("Иконка загружена: \(playIcon)")
+        self.view?.configurPlayTrackButton(image: playIcon)
+        self.view?.configurCoverImageView(image: UIImage(named: "Cover \(Int.random(in: 1...9))") ?? UIImage())
+        self.view?.configurTrackTimeSlider(value: 0)
+    }
+    
+    func addTimeObserver() {
+        let interval = CMTime(seconds: 0.1, preferredTimescale: CMTimeScale(NSEC_PER_SEC)) // Обновление каждые 0.1 секунды
+        timeObserver = audioPlayer?.addPeriodicTimeObserver(forInterval: interval, queue: .main) { [weak self] time in
+            guard let self = self else { return }
+            
+            // Обновляем значение слайдера
+            let duration = self.audioPlayer?.currentItem?.duration.seconds ?? 0
+            let currentTime = time.seconds
+            DispatchQueue.main.async {
+                self.view?.configurTrackTimeSlider(value: Float(currentTime / duration))
+                print(currentTime)
+            }
+        }
+    }
+    
+    
+    func tupOnPlayButton(){
+        
+        if isPlaing{
+            if newTrack{
+                guard let path = Bundle.main.path(forResource: "Metel", ofType: "mp3") else {
+                    print("Аудиофайл не найден")
+                    return
+                }
+                let url = URL(fileURLWithPath: path)
+                audioPlayer = AVPlayer(url: url)
+                newTrack = false
+                addTimeObserver()
+            }
+            audioPlayer?.play()
+            print(audioPlayer?.currentItem?.duration)
+            
+            view?.configurPlayTrackButton(image: Const.pauseButton)
+        }else{
+            audioPlayer?.pause()
+            view?.configurPlayTrackButton(image: Const.playButton)
+        }
         
     }
     
+    func changeTrackTime(partFullTime: Double){
+        guard let duration = audioPlayer?.currentItem?.duration.seconds else { return }
+                
+                let newTime = partFullTime * duration
+                let seekTime = CMTime(seconds: newTime, preferredTimescale: CMTimeScale(NSEC_PER_SEC))
+                audioPlayer?.seek(to: seekTime)
+    }
+}
+
+private extension PlayerViewPresenter{
+    enum Const{
+        static let playButton = UIImage(systemName: "play.fill") ?? UIImage()
+        static let pauseButton = UIImage(systemName: "pause.fill") ?? UIImage()
+    }
 }
